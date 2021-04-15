0882865128be6a8cbe8b691c62f28abcc05aa761
Matheus Augusto <matheus.augustosign@gmail.com>
2021-04-15 18:17:43 -0300

    Inicio Barra de Pesquisa

--- js/effect.js
+++ js/effect.js
@@ -2,3 +2,13 @@ const api ={
     key: "840f74b291c81f7dab17af38a197be5d",
     baseurl: "https://home.openweathermap.org/api_keys",  
 }
+
+const barrapesquisa = document.querySelector('.barra-pesquisa');
+barrapesquisa.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.kayCode ==13) {
+        getResults(barrapesquisa.value);
+        console.log(barrapesquisa.value);
+    }
+}
